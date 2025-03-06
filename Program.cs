f1b791bde7a7270bc9e2c562cc2dd9a1328edb55
Nihat774 <nihatum-ap216@gmail.com>
2025-03-05 21:30:34 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+#region TASK1
+//int userPrice = 50;
+//if (userPrice >= 91 && userPrice <= 100)
+//{
+//    Console.WriteLine("A aldiniz");
+//}
+//else if (userPrice >= 81 && userPrice < 91)
+//{
+//    Console.WriteLine("B aldiniz");
+//}
+//else if (userPrice >= 71 && userPrice < 81)
+//{
+//    Console.WriteLine("C aldiniz");
+//}
+//else if (userPrice >= 61 && userPrice < 71)
+//{
+//    Console.WriteLine("D aldiniz");
+//}
+//else if (userPrice >= 51 && userPrice<61)
+//{
+//    Console.WriteLine("E aldiniz");
+//}
+//else if (userPrice >= 0 && userPrice < 51)
+//{
+//    Console.WriteLine("Kesildiniz");
+//}
+//else
+//{
+//    Console.WriteLine("Balinizi dogru daxil edin!");
+//}
+#endregion TASK 2
+#region TASK 2
+//int number1 = 4;
+//int number2 = 4;
+//if (number1 % 2 == 0 && number2 % 2 == 0)
+//{
+//    Console.WriteLine(number1 + number2);
+//}
+//else
+//{
+//    Console.WriteLine("cem eded daxil edin");
+//}
+#endregion
+#region TASK3
+//int salary = 1000;
+//int credit = 200;
+//if (salary*0.6<credit) {
+//    Console.WriteLine("Kredit goture bilmezsiniz!");
+//}
+//else
+//{
+//    Console.WriteLine("kredit goture bilersiniz!");
+//}
+
+#endregion
+#region TASK 4
+//int reqem = 62;
+//if (reqem / 10 > reqem % 10)
+//{
+//    Console.WriteLine($"{reqem / 10} {reqem % 10} den boyukdur ");
+//}
+//else
+//{
+//    Console.WriteLine($"{reqem / 10} + {reqem % 10}den kicikdir. ");
+//}
+#endregion
+#region TASK 5
+//int en = 20;
+//int uzunluq = 30;
+//int sahe = en * uzunluq;
+//int perimetr = 2 * (en + uzunluq);
+//Console.WriteLine("sahe:" + sahe);
+//Console.WriteLine("perimetr:" + perimetr);
+#endregion
+#region TASK6
+//int number1 = 5;
+//int number2 = 6;
+//int newNumber1 = number2; //6
+//int newNumber2 = number1; //5
+//number1 = newNumber1; //6
+//number2 = newNumber2; //5
+//Console.WriteLine("number1:" + number1);
+//Console.WriteLine("number2:"+number2);
+#endregion
